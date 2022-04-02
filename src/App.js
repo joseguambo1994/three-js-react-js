05508cecd1b4827e74caa87133918743001be0e0
jose <jlguambo@gmail.com>
2022-04-01 23:01:22 -0500

    Add boxes, spheres and Canvas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 import './App.css';
+
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+function Sphere(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x -= 0.01))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 2.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+      visible userData={{ hello: 'world' }} rotation={[Math.PI / 2, 0, 0]}>
+      <sphereGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'green' : 'purple'} transparent />
+    </mesh>
+  )
+}
 
 function App() {
   return (
+    //     <div style={{flex:1, backgroundColor:'red'}}>
+
+    //     </div>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Canvas style={{ flex: 1, backgroundColor: 'blue' }}>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[-2.2, 0, 0]} />
+          <Box position={[2.2, 0, 0]} />
+          <Box position={[4.2, 0, 0]} />
+          <Box position={[-4.2, 0, 0]} />
+          <Sphere position={[0, 0, 0]} />
+          <Sphere position={[2, 2, 2]} />
+          <Sphere position={[2, -2, 2]} />
+          <Sphere position={[0, 2, 0]} />
+        </Canvas>
       </header>
     </div>
   );
